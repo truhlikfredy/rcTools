4e96ec3a6555b5fc9e1666e232ec072537f0e2df
Anton Krug <antonkrug@gmail.com>
2016-04-02 12:13:27 +0100

    Working proof of concept

---
+++ 1pinDpad/snippets/proofOfConcept/proofOfConcept.ino
@@ -0,0 +1,31 @@
+int sensorPin = A0;    
+int sensorValue =0;
+unsigned int i = 0, match =0, min = 0;
+
+void setup() {
+  Serial.begin(9600);
+  digitalWrite(A0, HIGH); 
+}
+
+int directions[] = {1001 , 210, 650, 311, 233, 65535, 368, 190, 483, 270  };
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  sensorValue = sensorValue + analogRead(sensorPin);
+  sensorValue = sensorValue + analogRead(sensorPin);
+  sensorValue = sensorValue + analogRead(sensorPin);
+  sensorValue = sensorValue / 4;
+
+  min = 65535;
+
+  for (i=0; i<10; i++) {
+    int diff = abs(sensorValue - directions[i]);
+    if ( diff < min ) {
+      match = i;
+      min = diff;
+    }
+  }
+  Serial.println(match);
+  
+  delay(20);
+}
