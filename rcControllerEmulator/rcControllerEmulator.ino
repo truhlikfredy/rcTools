bec4dd57832709b1688679302ab27415e71f0ded
Anton Krug <antonkrug@gmail.com>
2016-05-15 21:03:14 +0100

    Added button A could switch between FC and FTDI boards. And button B for future feature like LCD menu

--- rcControllerEmulator/rcControllerEmulator.ino
+++ rcControllerEmulator/rcControllerEmulator.ino
@@ -1,20 +1,24 @@
 //comment out to disable serial output
 //#define serialSpeed  9600
 
-#include <PpmPwm.h>
-#include <DPad1.h>
+#define SWITCH_A     9         //mode A button
+#define SWITCH_B     8         //programer switch button
+#define OUTPUT_FC    7         //OSD will communicate with FC
+#define OUTPUT_FTDI  6         //OSD will communicate with FTDI usb converter
+#define sensorPinA   A0        //dpad Left
+#define sensorPinB   A1        //dpad Right
+
+#include <PpmPwm.h>            //https://github.com/truhlikfredy/rcTools/tree/master/PpmPwm
+#include <DPad1.h>             //https://github.com/truhlikfredy/rcTools/tree/master/1pinDpad/DPad1
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>      //https://github.com/adafruit/Adafruit-GFX-Library
+#include <Adafruit_SSD1306.h>  //https://github.com/adafruit/Adafruit_SSD1306
+#include <Debounce.h>          //https://github.com/truhlikfredy/rcTools/tree/master/Debounce a modified http://playground.arduino.cc/Code/Debounce
 
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
-
-
-#define sensorPinA   A0
-#define sensorPinB   A1
 
 
 // Because I messed up and I run out of 10k resitors I used two 22k in paraler and then
@@ -27,23 +31,43 @@ unsigned int  directionsB[] = {1010 , 419, 488, 366, 757, 65535, 601, 276, 306, 
 DPad1 left (sensorPinA, &directionsA[0]);
 DPad1 right(sensorPinB, &directionsB[0]);
 PpmPwm pwm(4, 10, 11, 12, 13);
+Debounce debA = Debounce( 20 , SWITCH_A ); 
+Debounce debB = Debounce( 20 , SWITCH_B ); 
 
 
 void setup() {
+  //setup serial output
   #if defined(serialSpeed)
   Serial.begin(serialSpeed);
   #endif
 
+  //setup push button debouncers
+  pinMode(     SWITCH_A,    INPUT );
+  digitalWrite(SWITCH_A,    HIGH  );
+  
+  pinMode(     SWITCH_B,    INPUT );
+  digitalWrite(SWITCH_B,    HIGH  );
+
+  //setup outputs
+  pinMode(     OUTPUT_FC,   OUTPUT);
+  digitalWrite(OUTPUT_FC,   LOW   );
+
+  pinMode(     OUTPUT_FTDI, OUTPUT);
+  digitalWrite(OUTPUT_FTDI, LOW   );
+
+  //setup pwm outputs
   pwm.updatePortMs(0,500);   
   pwm.updatePortMs(1,500);   
   pwm.updatePortMs(2,500);   
   pwm.updatePortMs(3,500);  
   pwm.initializePwmOutput();
-  
+
+  //setup dpads internal pull-ups 
   // Set sensor pins to pull ups if you are not using external pull-ups
   //  digitalWrite(sensorPinA, HIGH); 
   //  digitalWrite(sensorPinB, HIGH); 
 
+  //setup LCD
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x78 (for the 128x32)
   display.clearDisplay();
@@ -60,12 +84,24 @@ void setup() {
 
 
 void loop() { 
-  static unsigned int chThr=0, chYaw=0 , chPitch=0, chRoll=0;
+  static unsigned int chThr   = 0, 
+                      chYaw   = 0, 
+                      chPitch = 0, 
+                      chRoll  = 0;
+                      
+  static boolean      btnA    = false,
+                      btnB    = false,
+                      osdFc   = true;
+
+  //read dpad states
   left .aquireState();
   right.aquireState();
 
+  btnA = debA.update();
+  btnB = debB.update();
+
   //when any state changed update values and graphics for both
-  if (left.isStateChanged() || right.isStateChanged()) {
+  if (left.isStateChanged() || right.isStateChanged() || btnA || btnB) {
 
     //Print out states to serial line (it's not needed but it doesn't hurt)
     #if defined(serialSpeed)
@@ -74,6 +110,26 @@ void loop() {
     Serial.print(right.getState());
     Serial.println();
     #endif
+
+    //switch state between OSD comunicating with FC and FTDI chip
+    if ( btnA && debA.read()==0 ) {
+      osdFc = !osdFc;
+
+      //turn off both states for moment and wait for the relays / transistors to finish transition
+      digitalWrite(OUTPUT_FC,   LOW );
+      digitalWrite(OUTPUT_FTDI, LOW );
+      delay(100);
+
+      //set the outpus depending on the state
+      if (osdFc) {
+        digitalWrite(OUTPUT_FC,   HIGH);
+        digitalWrite(OUTPUT_FTDI, LOW );
+      }
+      else {
+        digitalWrite(OUTPUT_FC,   LOW );
+        digitalWrite(OUTPUT_FTDI, HIGH);        
+      }
+    }
 
     //transfer the left dPad state into PPM type PWM values
     switch (left.getState()) {
@@ -112,22 +168,25 @@ void loop() {
     //update OLED display
     display.clearDisplay();
     display.setCursor(0,0);
-    display.print("Left:    ");  display.println(left.getState() );
-    display.print("Right:   ");  display.println(right.getState());
-    display.print("chThr:   ");  display.println(chThr  );
-    display.print("chYaw:   ");  display.println(chYaw  );
-    display.print("chPitch: ");  display.println(chPitch);
-    display.print("chRoll:  ");  display.println(chRoll );
+    display.print("Left:    ");  display.println(left.getState()    );
+    display.print("Right:   ");  display.println(right.getState()   );
+    display.print("chThr:   ");  display.println(chThr              );
+    display.print("chYaw:   ");  display.println(chYaw              );
+    display.print("chPitch: ");  display.println(chPitch            );
+    display.print("chRoll:  ");  display.println(chRoll             );
+    display.print("BtnA:    ");  display.print(  debA.read()        );
+    display.print(" BtnB:   ");  display.println(debB.read()        );
+    display.print("OSD com: ");  display.println((osdFc?"FC":"FTDI"));
 
     //draw the PPM lines for each chanell
-    display.drawLine(80,2*8+4, map(chThr,  1000,2000,85,128), 2*8+4, WHITE);
-    display.drawLine(80,3*8+4, map(chYaw,  1000,2000,85,128), 3*8+4, WHITE);
-    display.drawLine(80,4*8+4, map(chPitch,1000,2000,85,128), 4*8+4, WHITE);
-    display.drawLine(80,5*8+4, map(chRoll, 1000,2000,85,128), 5*8+4, WHITE);
+    display.drawLine(80, 2*8+4, map(chThr,  1000, 2000, 85, 128), 2*8+4, WHITE);
+    display.drawLine(80, 3*8+4, map(chYaw,  1000, 2000, 85, 128), 3*8+4, WHITE);
+    display.drawLine(80, 4*8+4, map(chPitch,1000, 2000, 85, 128), 4*8+4, WHITE);
+    display.drawLine(80, 5*8+4, map(chRoll, 1000, 2000, 85, 128), 5*8+4, WHITE);
 
     //try to visualy represent dPad directions
-    display.drawLine(80+12,8, 80+12+ map(chYaw, 1000,2000,-10,+10) , 8 + map(chThr,  1000,2000,+6,-6), WHITE);
-    display.drawLine(80+36,8, 80+36+ map(chRoll,1000,2000,-10,+10) , 8 + map(chPitch,1000,2000,+6,-6), WHITE);
+    display.drawLine(80+12, 8, 80+12+ map(chYaw,  1000, 2000, -10, +10) , 8 + map(chThr,   1000, 2000, +6, -6), WHITE);
+    display.drawLine(80+36, 8, 80+36+ map(chRoll, 1000, 2000, -10, +10) , 8 + map(chPitch, 1000, 2000, +6, -6), WHITE);
         
     display.display();
   }
